0032802db3727124994468bd2b392f0df9e8f3a8
Đàm Mạnh Đức <ducdm02@ominext.com>
2020-05-13 15:40:11 +0700

    print order

---
+++ PrintOrder/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace PrintOrder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        shop_drugEntities context = new shop_drugEntities();
+        CollectionViewSource ordViewSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ordViewSource = ((CollectionViewSource)(FindResource("orderViewSource")));
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource orderViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("orderViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // orderViewSource.Source = [generic data source]            
+            context.Orders.Load();
+            ordViewSource.Source = context.Orders.Local;
+        }
+    }
+}
